629e3cceb2607f0e1e7ab21c57980d0608c10236
Bauer <oslavalgooglemail.com>
2020-09-27 15:01:57 +0200

    initial

--- app/src/androidTest/java/com/example/android_build_example/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/android_build_example/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.android_build_example", appContext.getPackageName());
+        assertEquals("com.example.android_build_example1", appContext.getPackageName());
     }
 }
